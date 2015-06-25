c1d01c1ff34ee0314e8d2d88999918841bd4b19f
Pietro Incardona <incardon@mpi-cbg.de>
2015-06-25 18:51:58 +0200

    Adding missing files

---
+++ src/unit_test_init_cleanup.hpp
@@ -0,0 +1,24 @@
+/*
+ * unit_test_init_cleanup.hpp
+ *
+ *  Created on: Apr 17, 2015
+ *      Author: Pietro Incardona
+ */
+
+#ifndef UNIT_TEST_INIT_CLEANUP_HPP_
+#define UNIT_TEST_INIT_CLEANUP_HPP_
+
+#include "VCluster.hpp"
+
+struct ut_start {
+    ut_start()   { BOOST_MESSAGE("Initialize global VCluster"); init_global_v_cluster(&boost::unit_test::framework::master_test_suite().argc,&boost::unit_test::framework::master_test_suite().argv); }
+    ~ut_start()  { BOOST_MESSAGE("Delete global VClster");delete(global_v_cluster); }
+};
+
+//____________________________________________________________________________//
+
+BOOST_GLOBAL_FIXTURE( ut_start );
+
+
+
+#endif /* UNIT_TEST_INIT_CLEANUP_HPP_ */
