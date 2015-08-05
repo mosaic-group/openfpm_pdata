77a50a787994291e03a0b8dcb3b8143c075c28f3
Pietro Incardona <incardon@mpi-cbg.de>
2015-08-05 21:38:56 +0200

    Modified Metis installation script

--- install_Metis.sh
+++ install_Metis.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 mkdir ${HOME}/METIS
-wget http://ppmcore.mpi-cbg.de/web/metis-5.1.0.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/metis-5.1.0.tar.gz
 tar -xf metis-5.1.0.tar.gz
 cd metis-5.1.0.tar.gz
 sh ./configure --prefix={$HOME}/METIS
