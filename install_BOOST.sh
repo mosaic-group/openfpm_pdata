aad83a0e5a1257f6ba997cbd78dca358f749c714
Pietro Incardona <incardon@mpi-cbg.de>
2015-08-06 19:38:32 +0200

    Adding BOOST installation script

---
+++ install_BOOST.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# check if the directory ${HOME}/MPI exist
+
+if [ -d "${HOME}/BOOST" ]; then
+  echo "BOOST already installed"
+  exit 0
+fi
+
+mkdir ${HOME}/BOOST
+wget http://ppmcore.mpi-cbg.de/upload/boost_1_58_0.tar.bz2
+tar -xvf boost_1_58_0.tar.bz2
+cd boost_1_58_0
+./bootstrap.sh
+./b2 install --prefix=$HOME/BOOST
+
