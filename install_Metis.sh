6531e554daa9fdef2695298e659646074a145dbb
Pietro Incardona <incardon@mpi-cbg.de>
2015-08-06 11:03:16 +0200

    Fixing compilation script

--- install_Metis.sh
+++ install_Metis.sh
@@ -3,7 +3,8 @@
 mkdir ${HOME}/METIS
 wget http://ppmcore.mpi-cbg.de/upload/metis-5.1.0.tar.gz
 tar -xf metis-5.1.0.tar.gz
-cd metis-5.1.0.tar.gz
-sh ./configure --prefix={$HOME}/METIS
+cd metis-5.1.0
+make config shared=1 prefix=${HOME}/METIS
 make
 make install
+
