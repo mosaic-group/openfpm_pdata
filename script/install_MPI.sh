7368581a7fb279a001a02672697b39915c6f22c4
Pietro Incardona <incardon@mpi-cbg.de>
2015-08-19 15:06:36 +0200

    Fixing install script

--- script/install_MPI.sh
+++ script/install_MPI.sh
@@ -7,10 +7,10 @@ if [ -d "${HOME}/MPI" ]; then
   exit 0
 fi
 
-mkdir ${HOME}/MPI
 wget http://www.open-mpi.de/software/ompi/v1.8/downloads/openmpi-1.8.7.tar.bz2
 tar -xvf openmpi-1.8.7.tar.bz2
 cd openmpi-1.8.7
 sh ./configure --prefix=${HOME}/MPI --enable-opal-multi-threads --enable-mpi-f90
 make -j 4
+mkdir ${HOME}/MPI
 make install
