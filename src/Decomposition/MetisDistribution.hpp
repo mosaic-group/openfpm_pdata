aad5c2dd9ef562149d2cca46502748dc77627d02
Pietro Incardona <i-bird@localhost.localdomain>
2015-11-19 11:07:35 -0500

    Start refactoization

---
+++ src/Decomposition/MetisDistribution.hpp
@@ -0,0 +1,15 @@
+/*
+ * MetisDistribution.hpp
+ *
+ *  Created on: Nov 19, 2015
+ *      Author: i-bird
+ */
+
+#ifndef SRC_DECOMPOSITION_METISDISTRIBUTION_HPP_
+#define SRC_DECOMPOSITION_METISDISTRIBUTION_HPP_
+
+
+
+
+
+#endif /* SRC_DECOMPOSITION_METISDISTRIBUTION_HPP_ */
