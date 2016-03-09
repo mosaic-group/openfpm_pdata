c472c0d77ab6fbabe7ebe7250663f1f8b12ee628
Pietro Incardona <i-bird@localhost.localdomain>
2016-03-09 06:47:54 -0500

    Adding installation of PARMETIS

---
+++ script/install_Parmetis.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# check if the directory $1/PARMETIS exist
+
+if [ -d "$1/PARMETIS" ]; then
+  echo "PARMETIS already installed"
+  exit 0
+fi
+
+wget http://ppmcore.mpi-cbg.de/upload/parmetis-4.0.3.tar.gz
+tar -xf parmetis-4.0.3.tar.gz
+cd parmetis-4.0.3
+make -j 4
+if [ $? -eq 0 ]; then
+    echo "PARMETIS error installing"
+    exit 0
+fi
+mkdir $1/PARMETIS
+make install
+
