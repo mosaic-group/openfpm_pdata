16b6959f90c2845ef9dfb66d35c9cf7df4f52524
Pietro Incardona <i-bird@localhost.localdomain>
2016-03-15 15:06:13 +0100

    Fixing installation

--- build_pdata.sh
+++ build_pdata.sh
@@ -260,6 +260,11 @@ else
  source ~/.bashrc
  ./install -s
 
+ mpirun -np 1 ./src/pdata
+ if [ $? -ne 0 ]; then
+   curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to complete the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
+   exit 1 ;
+ fi
  mpirun -np 2 ./src/pdata
  if [ $? -ne 0 ]; then 
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to complete the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
