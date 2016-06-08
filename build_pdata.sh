d68964b8e48054c44adc93ec82f52edf6ed1d262
Pietro Incardona <i-bird@private-incardon-3.mpi-cbg.de>
2016-06-08 11:10:57 +0200

    Small change in the pdata build

--- build_pdata.sh
+++ build_pdata.sh
@@ -22,42 +22,42 @@ fi
 mkdir openfpm_numerics/src/config
 
 # pull from all the projects
-cd openfpm_data
-git checkout develop
-mkdir src/config
-git pull origin develop
-if [ $? -ne 0 ]; then
-  echo -e "Configure\033[91;5;1m FAILED \033[0m"
-  exit 1
-fi
-cd ..
+#cd openfpm_data
+#git checkout develop
+#mkdir src/config
+#git pull origin develop
+#if [ $? -ne 0 ]; then
+#  echo -e "Configure\033[91;5;1m FAILED \033[0m"
+#  exit 1
+#fi
+#cd ..
 
-cd openfpm_devices
-mkdir src/config
-git pull origin master
-if [ $? -ne 0 ]; then
-  echo -e "Configure\033[91;5;1m FAILED \033[0m"
-  exit 1
-fi
-cd ..
+#cd openfpm_devices
+#mkdir src/config
+#git pull origin master
+#if [ $? -ne 0 ]; then
+#  echo -e "Configure\033[91;5;1m FAILED \033[0m"
+#  exit 1
+#fi
+#cd ..
 
-cd openfpm_vcluster
-mkdir src/config
-git pull origin master
-if [ $? -ne 0 ]; then
-  echo -e "Configure\033[91;5;1m FAILED \033[0m"
-  exit 1
-fi
-cd ..
+#cd openfpm_vcluster
+#mkdir src/config
+#git pull origin master
+#if [ $? -ne 0 ]; then
+#  echo -e "Configure\033[91;5;1m FAILED \033[0m"
+#  exit 1
+#fi
+#cd ..
 
-cd openfpm_io
-mkdir src/config
-git pull origin master
-if [ $? -ne 0 ]; then
-  echo -e "Configure\033[91;5;1m FAILED \033[0m"
-  exit 1
-fi
-cd ..
+#cd openfpm_io
+#mkdir src/config
+#git pull origin master
+#if [ $? -ne 0 ]; then
+#  echo -e "Configure\033[91;5;1m FAILED \033[0m"
+#  exit 1
+#fi
+#cd ..
 
 if [ "$2" == "gin" ]
 then
@@ -72,6 +72,10 @@ then
  fi
 
  make install
+
+ if [ $3 -eq x"no_test" ]; then
+   exit 0;
+ fi
 
  if [ $? -ne 0 ]; then
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to complete the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
@@ -270,6 +274,10 @@ else
  if [ $? -ne 0 ]; then
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to complete the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
    exit 1 ;
+ fi
+
+ if [ $3 -eq x"no_test" ]; then
+   exit 0;
  fi
 
  mpirun -np 1 ./src/pdata
