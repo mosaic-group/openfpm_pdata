a12cbffb49edd4dde2a4e2ade56246f0decb5134
Pietro Incardona <incardon@mpi-cbg.de>
2016-10-29 11:24:11 +0200

    Eliminating success message

--- build.sh
+++ build.sh
@@ -113,5 +113,4 @@ else
 
 fi
 
-curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Info:\", \"color\": \"#00FF00\", \"text\":\"$2 completed succeffuly the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
 
