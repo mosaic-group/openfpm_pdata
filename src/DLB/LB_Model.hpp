9034c2b19eaf91a827028df11522f814f9fa6a28
Pietro Incardona <incardon@mpi-cbg.de>
2017-01-31 22:55:42 +0100

    Adding missing files

---
+++ src/DLB/LB_Model.hpp
@@ -0,0 +1,58 @@
+/*
+ * LB_Model.hpp
+ *
+ *  Created on: Jan 18, 2017
+ *      Author: i-bird
+ */
+
+#ifndef SRC_DLB_LB_MODEL_HPP_
+#define SRC_DLB_LB_MODEL_HPP_
+
+/*! \brief Linear model
+ *
+ * The linear model count each particle as weight one
+ *
+ */
+struct ModelLin
+{
+	size_t factor = 1;
+
+	ModelLin(size_t factor)
+	:factor(factor)
+	{}
+
+	ModelLin()	{}
+
+	template<typename Decomposition, typename vector> inline void addComputation(Decomposition & dec, const vector & vd, size_t v, size_t p)
+	{
+		dec.addComputationCost(v, 1);
+	}
+
+	template<typename Decomposition> inline void applyModel(Decomposition & dec, size_t v)
+	{
+		dec.setSubSubDomainComputationCost(v, dec.getSubSubDomainComputationCost(v));
+	}
+};
+
+/*! \brief Linear model
+ *
+ * The linear model count each particle as weight one
+ *
+ */
+struct ModelSquare
+{
+	size_t factor = 1;
+
+	template<typename Decomposition, typename vector> inline void addComputation(Decomposition & dec, const vector & vd, size_t v, size_t p)
+	{
+		dec.addComputationCost(v, 1);
+	}
+
+	template<typename Decomposition> inline void applyModel(Decomposition & dec, size_t v)
+	{
+		dec.setSubSubDomainComputationCost(v, dec.getSubSubDomainComputationCost(v) * dec.getSubSubDomainComputationCost(v));
+	}
+};
+
+
+#endif /* SRC_DLB_LB_MODEL_HPP_ */
