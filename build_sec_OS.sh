a34deb81eb721576d34d42f6c2769360c94e4c46
Pietro Incardona <incardon@mpi-cbg.de>
2017-07-13 10:50:31 +0200

    Adding secondary OS support

---
+++ build_sec_OS.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Make a directory in /tmp/OpenFPM_pdata
+
+echo "Directory: $1"
+echo "Machine: $2"
+
+mkdir src/config
+
+
+if [ "$2" == "windows10" ]; then
+    echo "Compiling on windows10"
+
+    echo "1" > input_install
+    echo "1" >> input_install
+    echo "1" >> input_install
+    echo "2" >> input_install
+    echo "y" >> input_install
+
+    ./install -i "/scratch/p_ppm/openfpm_deps_intel" < input_install
+
+fi
+
+
