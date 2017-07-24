db33fad9a6c9c8b0ffa11c497e3fe7aa6b516ff8
Pietro Incardona <incardon@mpi-cbg.de>
2017-07-25 01:17:41 +0200

    Fixing installation of zlib

--- script/install_HDF5.sh
+++ script/install_HDF5.sh
@@ -12,14 +12,14 @@ if [ -d "$1/HDF5" ]; then
 fi
 
 if [ ! -d "$1/ZLIB"  -a x"$platform" != x"cygwin" ]; then
-  rm zlib1211.tar.gz
+  rm zlib-1.2.11.tar.gz
   rm -rf zlib-1.2.11
   wget https://zlib.net/zlib-1.2.11.tar.gz
   if [ $? -ne 0 ]; then
     echo -e "\033[91;5;1m FAILED! Installation requires an Internet connection \033[0m"
     exit 1
   fi
-  tar -xf zlib1211.tar.gz
+  tar -xf zlib-1.2.11.tar.gz
   cd zlib-1.2.11
 
   CC=mpicc ./configure --prefix=$1/ZLIB
