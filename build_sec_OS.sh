ecf5149e979887ca7d34ec818fd6e57ad5f37d94
Pietro Incardona <incardon@mpi-cbg.de>
2017-07-24 13:41:43 +0200

    Fixing window10 branch name

--- build_sec_OS.sh
+++ build_sec_OS.sh
@@ -4,6 +4,7 @@
 
 echo "Directory: $1"
 echo "Machine: $2"
+echo "Branch: $BRANCH_NAME"
 
 mkdir src/config
 
@@ -12,13 +13,11 @@ if [ "$2" == "windows10" ]; then
     echo "Compiling on windows10"
 
     echo "1" > input_install
-    echo "1" >> input_install
-    echo "1" >> input_install
     echo "2" >> input_install
     echo "y" >> input_install
     echo "1" >> input_install
 
-    ./install -i "/scratch/p_ppm/openfpm_deps_intel" < input_install
+    ./install -i "/home/jenkins/$BRANCH_NAME" < input_install
 
 fi
 
