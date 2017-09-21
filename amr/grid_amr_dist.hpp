1373c4e35567ec2cd74f4cf0fc9f96778ed64aae
Pietro Incardona <incardon@mpi-cbg.de>
2017-09-21 03:07:09 +0200

    Adding Amr struct file

---
+++ amr/grid_amr_dist.hpp
@@ -0,0 +1,15 @@
+/*
+ * grid_amr_dist.hpp
+ *
+ *  Created on: Sep 21, 2017
+ *      Author: i-bird
+ */
+
+#ifndef AMR_GRID_AMR_DIST_HPP_
+#define AMR_GRID_AMR_DIST_HPP_
+
+
+
+
+
+#endif /* AMR_GRID_AMR_DIST_HPP_ */
