b7a45b31d05e28dbca4a6bca9bf89d2e16c7a3d5
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-05 23:43:35 +0100

    SSH pub print out

--- build.sh
+++ build.sh
@@ -109,6 +109,8 @@ else
   installation_dir="--prefix=$HOME/openfpm_dependencies/openfpm_pdata/$branch"
  fi
 
+ ssh -vvvT git@git.mpi-cbg.de
+
  mkdir $HOME/openfpm_dependencies/openfpm_pdata/$branch
  if [ x"$4" == x"full" ]; then
   ./install -i $HOME/$branch  -s -c "$installation_dir"
