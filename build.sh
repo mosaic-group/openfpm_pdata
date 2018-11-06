30d4ad3695f7b927ae9512c08e262efe88b81485
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-06 10:37:21 +0100

    Fixing SSH

--- build.sh
+++ build.sh
@@ -129,7 +129,7 @@ else
   source $HOME/openfpm_vars_$branch
   make $3
  else
-  echo "$installation_dir"
+  echo "Installing with: ./install -i $HOME/openfpm_dependencies/openfpm_pdata/$branch -m -s -c \"$installation_dir --no-recursion\""
   exit 1
   ./install -i $HOME/openfpm_dependencies/openfpm_pdata/$branch -m -s -c "$installation_dir --no-recursion"
   mv $HOME/openfpm_vars $HOME/openfpm_vars_$branch
