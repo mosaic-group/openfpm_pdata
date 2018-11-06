38ebe9d80ec93a3aaaacb256978ff11e52296c7a
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-06 10:40:24 +0100

    Fixing SSH

--- build.sh
+++ build.sh
@@ -109,13 +109,9 @@ else
   installation_dir="--prefix=$HOME/openfpm_install/$branch"
  fi
 
- #ssh -vvvT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null  git@git.mpi-cbg.de
- #ssh -vvvT -o StrictHostKeyChecking=no  -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null  git@git.mpi-cbg.de
  # force ssh to not use HostKey verification
- cat $HOME/.ssh/id_rsa.pub
  echo "StrictHostKeyChecking=no" > $HOME/.ssh/config
  chmod 600 $HOME/.ssh/config
- rm -rf $HOME/METIS
 
  mkdir $HOME/openfpm_dependencies/openfpm_pdata/$branch
  if [ x"$4" == x"full" ]; then
