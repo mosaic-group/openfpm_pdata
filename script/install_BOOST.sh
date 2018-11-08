a1df8b21690d764add6508ef1b15124f114ccf12
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-08 23:26:14 +0100

    Fixing boost requirements

--- script/install_BOOST.sh
+++ script/install_BOOST.sh
@@ -7,11 +7,11 @@ if [ -d "$1/BOOST" ]; then
   exit 0
 fi
 
-wget http://ppmcore.mpi-cbg.de/upload/boost_1_63_0.tar.bz2
-tar -xvf boost_1_63_0.tar.bz2
-cd boost_1_63_0
+wget http://ppmcore.mpi-cbg.de/upload/boost_1_68_0.tar.bz2
+tar -xvf boost_1_68_0.tar.bz2
+cd boost_1_68_0
 ./bootstrap.sh --with-toolset=$3
 mkdir $1/BOOST
 ./b2 -j $2 install --prefix=$1/BOOST
-rm -rf boost_1_63_0
+rm -rf boost_1_68_0
 
