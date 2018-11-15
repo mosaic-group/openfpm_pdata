02262f6646912b850a172d7ad8cb475c1cc4545c
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-15 10:06:21 +0100

    Fixing run on OSX

--- run.sh
+++ run.sh
@@ -64,7 +64,7 @@ else
 
  source $HOME/openfpm_vars_$7
 
- mpirun -np $4 ./build/src/pdata
+ mpirun --oversubscribe -np $4 ./build/src/pdata
  if [ $? -ne 0 ]; then
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$2 failed to complete the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
    exit 1 ;
