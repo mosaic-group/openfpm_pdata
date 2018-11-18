45c3ee49fd4d8f3070a57d5472a5ef75b3b04c5c
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-18 16:57:30 +0100

    updating MPI to 3.1.3

--- script/install_MPI.sh
+++ script/install_MPI.sh
@@ -8,7 +8,7 @@ if [ -d "$1/MPI" ]; then
 fi
 rm -rf openmpi-3.1.3
 rm openmpi-3.1.3.tar.gz
-wget http://ppmcore.mpi-cbg.de/upload/openmpi-3.1.1.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/openmpi-3.1.3.tar.gz
 tar -xvf openmpi-3.1.3.tar.gz
 cd openmpi-3.1.3
 
