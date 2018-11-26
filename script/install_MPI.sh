d5078ae843ce6742867719ed652cf5fad948c841
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-26 22:22:39 +0100

    Fixing install openmpi

--- script/install_MPI.sh
+++ script/install_MPI.sh
@@ -6,11 +6,11 @@ if [ -d "$1/MPI" ]; then
   echo "MPI already installed"
   exit 0
 fi
-rm -rf mpich-3.3
-rm mpich-3.3.tar.gz
-wget http://ppmcore.mpi-cbg.de/upload/mpich-3.3.tar.gz
-tar -xvf mpich-3.3.tar.gz
-cd mpich-3.3
+rm -rf openmpi-3.1.3
+rm openmpi-3.1.3.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/openmpi-3.1.3.tar.gz
+tar -xvf openmpi-3.1.3.tar.gz
+cd openmpi-3.1.3
 
 #
 #                  --disable-mca-dso \
