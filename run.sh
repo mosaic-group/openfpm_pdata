4211621f5fd0c3f28e1a64d3f5cfc4f8f0218572
Pietro Incardona <incardon@mpi-cbg.de>
2018-12-03 00:41:58 +0100

    Check if MPI is correct

--- run.sh
+++ run.sh
@@ -80,6 +80,8 @@ else
 
  source $HOME/openfpm_vars_$branch
 
+  mpirun --version
+
  mpirun $mpi_options -np $nproc ./build/src/pdata
  if [ $? -ne 0 ]; then
    curl -X POST --data "payload={\"icon_emoji\": \":jenkins:\", \"username\": \"jenkins\"  , \"attachments\":[{ \"title\":\"Error:\", \"color\": \"#FF0000\", \"text\":\"$hostname failed to complete the openfpm_pdata test \" }] }" https://hooks.slack.com/services/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
