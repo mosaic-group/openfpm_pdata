696c1ffa19b9d3efc7374df3f87432b60501da52
Pietro Incardona <incardon@mpi-cbg.de>
2018-12-13 14:16:59 +0100

    Deactivating again gitlab CI because broken

--- src/Vector/tests/vector_dist_dlb_test.hpp
+++ src/Vector/tests/vector_dist_dlb_test.hpp
@@ -302,12 +302,6 @@ template<typename vector_type> void test_dlb_vector()
 		vd.map();
 		vd.template ghost_get<>();
 
-		if (create_vcluster().rank() == 0)
-		{
-			std::cout << "Activating Crash " << std::endl;
-			return;
-		}
-
 		auto VV2 = vd.getVerlet(0.01);
 
 		auto it2 = vd.getDomainIterator();
