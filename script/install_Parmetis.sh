611ca10877b2726ca7eeb5501ec0c8961a593387
Pietro Incardona <incardon@mpi-cbg.de>
2020-07-17 00:49:57 +0200

    Fixing parmetis mirror

--- script/install_Parmetis.sh
+++ script/install_Parmetis.sh
@@ -15,7 +15,7 @@ rm -rf parmetis-4.0.3
 rm parmetis-4.0.3.tar.gz
 
 #wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
-http://ppmcore.mpi-cbg.de/upload/parmetis-4.0.3.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/parmetis-4.0.3.tar.gz
 tar -xf parmetis-4.0.3.tar.gz
 cd parmetis-4.0.3
 # Change to 64 bit parmetis
