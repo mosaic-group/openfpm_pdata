788e03c2ae0761a773c21795b460f37e231e4fbe
i-bird <i-bird@localhost.localdomain>
2021-02-02 15:35:04 +0100

    Fixing installation without cuda

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ if [ x"$hostname" == x"falcon1" ]; then
 	if [ x"$comp_type" == x"intel" ]; then
         	module load parallel_studio_xe/2019u1
 		dependency_dir=/projects/ppm/rundeck/openfpm_dependencies_intel/
-        elif [ x"$with_gpu" == x"0" ]; then
+        elif [ x"$with_gpu" == x"" ]; then
 		mkdir /projects/ppm/rundeck/openfpm_dependencies_${branch}_no_cuda/
                 dependency_dir=/projects/ppm/rundeck/openfpm_dependencies_${branch}_no_cuda/
 	else
