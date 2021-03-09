9771ac742ec92445f4259e353c25bff282afb2d1
Incardona Pietro <incardon@mpi-cbg.de>
2021-03-09 19:37:43 +0100

    Upgrading MPI

--- script/download_MPI.sh
+++ script/download_MPI.sh
@@ -2,8 +2,8 @@
 
 # check if the directory $1/MPI exist
 
-rm -rf openmpi-4.0.4
-rm openmpi-4.0.4.tar.gz
-wget http://ppmcore.mpi-cbg.de/upload/openmpi-4.0.4.tar.gz
-tar -xvf openmpi-4.0.4.tar.gz
+rm -rf openmpi-4.1.0
+rm openmpi-4.1.0.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/openmpi-4.1.0.tar.gz
+tar -xvf openmpi-4.1.0.tar.gz
 
