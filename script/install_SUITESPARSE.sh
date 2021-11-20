86fecff32e6bce1f20f7ccd9b970907c9aee1a79
Incardona Pietro <incardon@mpi-cbg.de>
2021-11-21 00:48:55 +0100

    Updating SuiteSparse 5.10

--- script/install_SUITESPARSE.sh
+++ script/install_SUITESPARSE.sh
@@ -6,21 +6,21 @@ source script/discover_os
 discover_os
 
 # check if the directory $1/SUITESPARSE exist
-rm -rf SuiteSparse-5.7.2
+rm -rf SuiteSparse-5.10.1
 if [ -d "$1/SUITESPARSE"  -a -f "$1/SUITESPARSE/include/umfpack.h" ]; then
   echo "SUITESPARSE is already installed"
   exit 0
 fi
 
-rm SuiteSparse-5.7.2.tar.gz
-wget http://ppmcore.mpi-cbg.de/upload/SuiteSparse-5.7.2.tar.gz
+rm SuiteSparse-5.10.1.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/SuiteSparse-5.10.1.tar.gz
 rm -rf SuiteSparse
-tar -xf SuiteSparse-5.7.2.tar.gz
+tar -xf SuiteSparse-5.10.1.tar.gz
 if [ $? != 0 ]; then
   echo "Failed to download SuiteSparse"
   exit 1
 fi
-cd SuiteSparse-5.7.2
+cd SuiteSparse-5.10.1
 
 if [ x"$CXX" == x"icpc" ]; then
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/$1/OPENBLAS/lib"
@@ -45,5 +45,5 @@ make library "CC=$CC" "CXX=$CXX" "CUDA=no" "INSTALL=$1/SUITESPARSE" "INSTALL_LIB
 echo "Making install"
 make install "CC=$CC" "CXX=$CXX" "CUDA=no" "INSTALL=$1/SUITESPARSE" "INSTALL_LIB=$1/SUITESPARSE/lib" "INSTALL_INCLUDE=$1/SUITESPARSE/include" "BLAS=-L$1/OPENBLAS/lib -lopenblas -pthread" "LAPACK="
 # Mark the installation
-echo 2 > $1/SUITESPARSE/version
-rm SuiteSparse-5.7.2.tar.gz
+echo 3 > $1/SUITESPARSE/version
+rm SuiteSparse-5.10.1.tar.gz
