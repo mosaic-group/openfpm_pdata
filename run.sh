0721acf0fdd3a880ce3d72639d5b54bd223ad521
Incardona Pietro <incardon@mpi-cbg.de>
2022-05-03 21:52:29 +0200

    Fixing run for root

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Make a directory in /tmp/OpenFPM_pdata
 
 workspace=$1
-hostname=$(hostname)
+hostname=$(cat hostname)
 nproc=$3
 ntask_per_node=$5
 nodes=$4
@@ -26,6 +26,9 @@ if [ x"$hostname" == x"cifarm-ubuntu-node.mpi-cbg.de"  ]; then
         mpi_options="--mca btl self,vader"
 fi
 
+if [ x"$hostname" == x"cifarm-centos-node"  ]; then
+        mpi_options="--allow-run-as-root"
+fi
 
 echo "Running general machine"
 if [ x"$hostname" == x"no_test" ]; then
