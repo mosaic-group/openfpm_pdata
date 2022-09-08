5b0c68170ab49d210262e3b038a7360965529571
gustav_elias_landfried.kraatz <gustav_elias_landfried.kraatz@tu-dresden.de>
2022-09-08 14:02:40 +0200

    Instance: skinIterator + skinOpenIterator

--- src/Formal_interface/Instance.hpp
+++ src/Formal_interface/Instance.hpp
@@ -38,6 +38,29 @@ protected:
         return boxIterator;
     }
 
+    template <unsigned int dimension, typename PositionType>
+    auto skinIterator(Point<dimension, PositionType> pointMin, Point<dimension, PositionType> pointMax, PositionType width,  size_t (& sz)[dimension]) {
+        Box<dimension, PositionType> drawOuterBox(pointMin, pointMax);
+        Box<dimension, PositionType> drawInnerBox(pointMin + width, pointMax - width);
+        Box<dimension, PositionType> domain (particleData.simulationParameters.domainMin, particleData.simulationParameters.domainMax);
+
+        auto boxIterator = DrawParticles::DrawSkin(particleData.getOpenFPMContainer(),sz,domain,drawInnerBox, drawOuterBox);
+        return boxIterator;
+    }
+
+    template <unsigned int dimension, typename PositionType>
+    auto skinOpenIterator(Point<dimension, PositionType> pointMin, Point<dimension, PositionType> pointMax, PositionType width,  size_t (& sz)[dimension]) {
+        Point<dimension, PositionType> upperCutoutPoint = pointMax - width;
+        upperCutoutPoint[dimension - 1] = pointMax[dimension - 1];
+
+        Box<dimension, PositionType> drawOuterBox(pointMin, pointMax);
+        Box<dimension, PositionType> drawInnerBox(pointMin + width, upperCutoutPoint);
+        Box<dimension, PositionType> domain (particleData.simulationParameters.domainMin, particleData.simulationParameters.domainMax);
+
+        auto boxIterator = DrawParticles::DrawSkin(particleData.getOpenFPMContainer(),sz,domain,drawInnerBox, drawOuterBox);
+        return boxIterator;
+    }
+
 public:
 
     Instance(ParticleData<ParticleMethodType, SimulationParametersType> &particleData_in) : particleData(particleData_in) {
