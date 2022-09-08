5d6372a1870763275280ab23e756fb789079c992
gustav_elias_landfried.kraatz <gustav_elias_landfried.kraatz@tu-dresden.de>
2022-09-08 14:02:59 +0200

    ParticleSignature.hpp documentation

--- src/Formal_interface/ParticleSignature.hpp
+++ src/Formal_interface/ParticleSignature.hpp
@@ -8,10 +8,24 @@
 #include "Vector/vector_dist.hpp"
 #include "DataContainer.hpp"
 
+
+/**
+ * Defines the structure of the particle model used in the simulation.
+ */
 struct ParticleSignature {
+
+    // Number of dimensions of the space
     static constexpr int dimension = 2;
+
+    // Data-type of the space / particle position
     typedef float position;
+
+    // Properties of the particle in their respective order
     typedef aggregate<float[dimension], float[dimension]> properties;
+
+    // Underlying data structure of the particles.
+    // FREE_PARTICLES for Lagrangian particles that can change their position
+    // MESH_PARTICLES for Eularian particles with fix positions
     typedef FREE_PARTICLES dataStructure;
 };
 
