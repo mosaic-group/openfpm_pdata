b97fe78d403fa96b7362a8b9d44f3dfab1875f70
gustav_elias_landfried.kraatz <gustav_elias_landfried.kraatz@tu-dresden.de>
2022-09-21 10:27:20 +0200

    add CellList templates PUSH, PULL_PUSH

--- src/Formal_interface/CellList.hpp
+++ src/Formal_interface/CellList.hpp
@@ -137,5 +137,119 @@ public:
 
 };
 
+template <typename ParticleMethodType, typename SimulationParametersType>
+class CellList_Impl<ParticleMethodType, SimulationParametersType, INTERACTION_PUSH> {
+
+    using ParticleSignatureType = typename ParticleMethodType::ParticleSignature;
+    static constexpr int dimension = ParticleSignatureType::dimension;
+    using PositionType = typename ParticleSignatureType::position;
+    using PropertyType = typename ParticleSignatureType::properties;
+
+    ParticleMethodType particleMethod;
+    SimulationParametersType simulationParameters;
+
+    using CellListType = typename vector_dist<dimension, PositionType, PropertyType>::CellList_type;
+
+    CellListType cellList;
+
+public:
+
+    explicit CellList_Impl(ParticleData<ParticleMethodType, SimulationParametersType> &particleData): cellList(
+            createCellList(particleData)) {
+
+    }
+
+    CellListType createCellList(ParticleData<ParticleMethodType, SimulationParametersType> &particleData) {
+        return particleData.getOpenFPMContainer().getCellList(simulationParameters.cellWidth);
+    }
+
+    void executeInteraction(ParticleData<ParticleMethodType, SimulationParametersType> &particleData) {
+
+        // update symmetric cell list
+        particleData.getOpenFPMContainer().updateCellList(cellList);
+
+        // iterate through all particles
+        auto iteratorAll = particleData.getParticleIterator();
+        while (iteratorAll.isNext())
+        {
+            auto p = iteratorAll.get();
+            Particle<ParticleSignatureType> particle(particleData.dataContainer, p);
+
+            // iterate through all particles in neighbor cells
+            auto iteratorNeighbors = cellList.template getNNIterator<NO_CHECK>(cellList.getCell(
+                    particleData.getOpenFPMContainer().getPos(p)));
+
+            while (iteratorNeighbors.isNext()) {
+                auto n = iteratorNeighbors.get();
+                Particle<ParticleSignatureType> neighbor(particleData.dataContainer, n);
+
+                if (particle != neighbor) {
+                    particleMethod.interact(particle, neighbor);
+                }
+                ++iteratorNeighbors;
+            }
+            ++iteratorAll;
+        }
+    }
+
+};
+
+template <typename ParticleMethodType, typename SimulationParametersType>
+class CellList_Impl<ParticleMethodType, SimulationParametersType, INTERACTION_PULL_PUSH> {
+
+    using ParticleSignatureType = typename ParticleMethodType::ParticleSignature;
+    static constexpr int dimension = ParticleSignatureType::dimension;
+    using PositionType = typename ParticleSignatureType::position;
+    using PropertyType = typename ParticleSignatureType::properties;
+
+    ParticleMethodType particleMethod;
+    SimulationParametersType simulationParameters;
+
+    using CellListType = typename vector_dist<dimension, PositionType, PropertyType>::CellList_type;
+
+    CellListType cellList;
+
+public:
+
+    explicit CellList_Impl(ParticleData<ParticleMethodType, SimulationParametersType> &particleData): cellList(
+            createCellList(particleData)) {
+
+    }
+
+    CellListType createCellList(ParticleData<ParticleMethodType, SimulationParametersType> &particleData) {
+        return particleData.getOpenFPMContainer().getCellList(simulationParameters.cellWidth);
+    }
+
+    void executeInteraction(ParticleData<ParticleMethodType, SimulationParametersType> &particleData) {
+
+        // update symmetric cell list
+        particleData.getOpenFPMContainer().updateCellList(cellList);
+
+        // iterate through all particles
+        auto iteratorAll = particleData.getParticleIterator();
+        while (iteratorAll.isNext())
+        {
+            auto p = iteratorAll.get();
+            Particle<ParticleSignatureType> particle(particleData.dataContainer, p);
+
+            // iterate through all particles in neighbor cells
+            auto iteratorNeighbors = cellList.template getNNIterator<NO_CHECK>(cellList.getCell(
+                    particleData.getOpenFPMContainer().getPos(p)));
+
+            while (iteratorNeighbors.isNext()) {
+                auto n = iteratorNeighbors.get();
+                Particle<ParticleSignatureType> neighbor(particleData.dataContainer, n);
+
+                if (particle != neighbor) {
+                    particleMethod.interact(particle, neighbor);
+                }
+                ++iteratorNeighbors;
+            }
+            ++iteratorAll;
+        }
+    }
+
+};
+
 
 #endif //OPENFPM_PDATA_CELLLIST_HPP
