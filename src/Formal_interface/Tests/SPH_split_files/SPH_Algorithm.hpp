df9257d11f3dabbcb908822130887170aa824366
gustav_elias_landfried.kraatz <gustav_elias_landfried.kraatz@tu-dresden.de>
2022-09-29 14:32:59 +0200

    SPH change property access syntax to _raw

--- src/Formal_interface/Tests/SPH_split_files/SPH_Algorithm.hpp
+++ src/Formal_interface/Tests/SPH_split_files/SPH_Algorithm.hpp
@@ -87,11 +87,11 @@ public:
 
     void interact(Particle<ParticleSignature> particle, Particle<ParticleSignature> neighbor) override {
 
-        Point<dimension, PositionType> n_pos = neighbor.position();
-        Point<dimension, PositionType> p_pos = particle.position();
+        Point<dimension, PositionType> n_pos = neighbor.position_raw();
+        Point<dimension, PositionType> p_pos = particle.position_raw();
         Point<dimension, PositionType> r_pq = n_pos - p_pos;
-        Point<dimension, PositionType> v_pq =  neighbor.template property_vec<velocity>()
-                                               -particle.template property_vec<velocity>();
+        Point<dimension, PositionType> v_pq =  neighbor.template property<velocity>()
+                                               -particle.template property<velocity>();
 //        double dist2_pq = r_pq.abs2();
         double dist2_pq = p_pos.distance2(n_pos);
         double dist_pq = sqrt(dist2_pq);
@@ -99,20 +99,20 @@ public:
         double f_pq=  pow(1.0-dist_pq/2.0/g.h , 3);
         double vr = scalarProduct(r_pq, v_pq);
 
-        double p_pressure_density2=pressure_density2(particle.template property_vec<density>());
-        double q_pressure_density2=pressure_density2(neighbor.template property_vec<density>());
+        double p_pressure_density2=pressure_density2(particle.template property<density>());
+        double q_pressure_density2=pressure_density2(neighbor.template property<density>());
         double interim01= p_pressure_density2+q_pressure_density2 ;
         double interim02 = 10*g.nu/dist2_pq * vr;
 
-        particle.template property_vec<deltaVelocity>() += (interim01-interim02/particle.template property_vec<density>())* r_pq * f_pq;
-        neighbor.template property_vec<deltaVelocity>() -= (interim01-interim02/neighbor.template property_vec<density>())* r_pq * f_pq;
+        particle.template property<deltaVelocity>() += (interim01-interim02/particle.template property<density>())* r_pq * f_pq;
+        neighbor.template property<deltaVelocity>() -= (interim01-interim02/neighbor.template property<density>())* r_pq * f_pq;
 
 
 
 double densityChange= vr * f_pq;
 
-particle.template property_vec<deltaDensity>() += densityChange;
-neighbor.template property_vec<deltaDensity>() += densityChange;
+particle.template property<deltaDensity>() += densityChange;
+neighbor.template property<deltaDensity>() += densityChange;
 
 
 }
@@ -120,46 +120,46 @@ neighbor.template property_vec<deltaDensity>() += densityChange;
 void evolve(Particle<ParticleSignature> particle) override {
 double prefact = g.mass* -5.0*21.0/16.0/M_PI/pow(g.h,5);
 
-Point<dimension, PositionType> acceleration = g.gravity + particle.template property_vec<deltaVelocity>()*prefact;
+Point<dimension, PositionType> acceleration = g.gravity + particle.template property<deltaVelocity>()*prefact;
 
-double densityacceleration = particle.template property_vec<deltaDensity>()*prefact;
+double densityacceleration = particle.template property<deltaDensity>()*prefact;
 
         if (g.phase==0){
-            if (particle.template property_vec<boundary>()==false){
+            if (particle.template property<boundary>()==false){
                 // fluid
 
                 // move particles half step
-                particle.template property_vec<positionOld>()=particle.position_vec();
-                particle.position_vec() += g.dt/2.0f*particle.template property_vec<velocity>();
+                particle.template property<positionOld>()=particle.position();
+                particle.position() += g.dt/2.0f*particle.template property<velocity>();
 
                 // change velocity half step
-                particle.template property_vec<velocityOld>()=particle.template property_vec<velocity>();
-                particle.template property_vec<velocity>()+= g.dt/2.0*acceleration;
+                particle.template property<velocityOld>()=particle.template property<velocity>();
+                particle.template property<velocity>()+= g.dt/2.0*acceleration;
             }
 
             // fluid + boundary
 
             // change density
-            particle.template property_vec<densityOld>()=particle.template property_vec<density>();
-            particle.template property_vec<density>()+=g.dt/2.0*densityacceleration;
+            particle.template property<densityOld>()=particle.template property<density>();
+            particle.template property<density>()+=g.dt/2.0*densityacceleration;
         }
         else{
-            if (particle.template property_vec<boundary>()==false){
+            if (particle.template property<boundary>()==false){
                 // fluid
 
                 // move particle from original position
                 Point<dimension, PositionType> step_acc_half = g.dt  / 2.0 * acceleration;
-                Point<dimension, PositionType> step_vel = g.dt * (particle.template property_vec<velocityOld>() + step_acc_half);
-                Point<dimension, PositionType> new_pos = particle.template property_vec<positionOld>() + step_vel;
-                particle.position_vec() = new_pos;
+                Point<dimension, PositionType> step_vel = g.dt * (particle.template property<velocityOld>() + step_acc_half);
+                Point<dimension, PositionType> new_pos = particle.template property<positionOld>() + step_vel;
+                particle.position() = new_pos;
 
                 // change velocity
                 Point<dimension, PositionType> step_acc_full = g.dt * acceleration;
-                particle.template property_vec<velocity>() = particle.template property_vec<velocityOld>() + step_acc_full;
+                particle.template property<velocity>() = particle.template property<velocityOld>() + step_acc_full;
 
 
-//                particle.position_vec() = particle.template property_vec<positionOld>() + g.dt * (particle.template property_vec<velocityOld>()  +  (g.dt  / 2.0) * acceleration);
-//                particle.template property_vec<velocity>() = particle.template property_vec<velocityOld>() +g.dt*acceleration;
+//                particle.position() = particle.template property<positionOld>() + g.dt * (particle.template property<velocityOld>()  +  (g.dt  / 2.0) * acceleration);
+//                particle.template property<velocity>() = particle.template property<velocityOld>() +g.dt*acceleration;
 
 
             }
@@ -167,12 +167,12 @@ double densityacceleration = particle.template property_vec<deltaDensity>()*pref
             // fluid + boundary
 
             // change density
-            particle.template property_vec<density>()= particle.template property_vec<densityOld>() + g.dt*densityacceleration;
+            particle.template property<density>()= particle.template property<densityOld>() + g.dt*densityacceleration;
         }
 
         //set to 0 to have a fresh accumulators for the next time step
-        particle.template property_vec<deltaVelocity>()=Point<dimension, PositionType> (0.0);
-        particle.template property_vec<deltaDensity>()=0.0;
+        particle.template property<deltaVelocity>()=Point<dimension, PositionType> (0.0);
+        particle.template property<deltaDensity>()=0.0;
     }
 
     void evolveGlobalVariable() override {
