c119074ed480a9c0b4cb07c172b9ae22bab98455
gustav_elias_landfried.kraatz <gustav_elias_landfried.kraatz@tu-dresden.de>
2022-11-14 14:59:25 +0100

    create distance2 for Particle objects

--- src/Formal_interface/Util.hpp
+++ src/Formal_interface/Util.hpp
@@ -6,11 +6,20 @@
 #define OPENFPM_PDATA_UTIL_HPP
 
 #include "Vector/vector_dist.hpp"
+#include "Formal_interface/Particle.hpp"
 
 
 template<typename positionType, unsigned int dimension>
 positionType abs2(Point<dimension, positionType> point) {
     return point.distance2(point.zero_p());
 }
+
+template<typename ParticleSignatureType>
+typename ParticleSignatureType::position distance2(Particle<ParticleSignatureType> particle, Particle<ParticleSignatureType> neighbor) {
+    Point<ParticleSignatureType::dimension, typename ParticleSignatureType::position> p_pos = particle.position_raw();
+    Point<ParticleSignatureType::dimension, typename ParticleSignatureType::position> n_pos = neighbor.position_raw();
+    return p_pos.distance2(n_pos);
+}
+
 
 #endif //OPENFPM_PDATA_UTIL_HPP
