efdf7e4de720260ff28e7403482df8cae446fd5d
gustav_elias_landfried.kraatz <gustav_elias_landfried.kraatz@tu-dresden.de>
2022-12-12 11:23:12 +0100

    reduce particle number DEM benchmark

--- src/Formal_interface/Benchmarks/ElasticCollision_DEM/FI_parallel_version.hpp
+++ src/Formal_interface/Benchmarks/ElasticCollision_DEM/FI_parallel_version.hpp
@@ -36,7 +36,7 @@ struct GlobalVariable {
     float r_cut = 0.08;
     float damp = 0.9;
     float domainSize = 9.0;
-    int number_particles = 200000;
+    int number_particles = 50000;
 } globalvar;
 
 
