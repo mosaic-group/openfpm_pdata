89d0d5b571515ecf5c926b121024bb07bcb139ec
absingh <absingh@mpi-cbg.de>
2023-02-10 10:56:33 +0100

    MacOS x86 boost gcc fix for installation

--- script/install_BOOST.sh
+++ script/install_BOOST.sh
@@ -37,7 +37,6 @@ else
 	./bootstrap.sh --with-toolset=$3
 fi
 
-
 mkdir $1/BOOST
 # Several flavours
 if [ x"$platform" == x"osx" ]; then
@@ -48,7 +47,11 @@ if [ x"$platform" == x"osx" ]; then
             ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=arm abi=aapcs binary-format=mach-o toolset=$3  -sNO_LZMA=1 -sNO_ZSTD=1
         fi
     else
-        ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=x86 abi=sysv binary-format=mach-o toolset=clang  -sNO_LZMA=1 -sNO_ZSTD=1
+      if [ x"$3" == x"" ]; then
+          ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=x86 abi=sysv binary-format=mach-o toolset=clang  -sNO_LZMA=1 -sNO_ZSTD=1
+        else
+          ./b2 -a -j $2 install --prefix=$1/BOOST address-model=64 architecture=x86 abi=sysv binary-format=mach-o toolset=gcc  -sNO_LZMA=1 -sNO_ZSTD=1
+      fi
     fi
 else
     ./b2 -a -j $2 install --prefix=$1/BOOST  -sNO_LZMA=1 -sNO_ZSTD=1
