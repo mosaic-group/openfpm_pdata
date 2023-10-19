9e6da20df68cef029a96326f50751a14e35bbe56
Serhii Yaskovets <yaskovet@mpi-cbg.de>
2023-10-19 20:56:20 +0200

    Move example CC/NVCC parameters to common.mk

--- example/common.mk
+++ example/common.mk
@@ -1,4 +1,5 @@
 # SET ENVIRONMENT VARIABLES ACCORDING TO ONE OF THE SUPPORTED MODES (cpu, gpu-nvcc, gpu-hip, gpu-emulated (CUDA_ON_CPU))
+# openfpm installed and the gpu examples have to be compiled in the same mode (i.e. via nvcc, hip or gpu-emulated (CUDA_ON_CPU))
 
 ### This is a trick to avoid "Command not found if you no not have NVCC compiler". In practice the normal C++ compiler is used
 ### internally the example disable with the preprocessor its code if not compiled with nvcc 
@@ -7,29 +8,30 @@ CUDA_CC_LINK=
 
 ifdef HIP
     CUDA_CC=hipcc
-    CUDA_OPTIONS=-D__NVCC__ -D__HIP__ -DCUDART_VERSION=11000 -D__CUDACC__ -D__CUDACC_VER_MAJOR__=11 -D__CUDACC_VER_MINOR__=0 -D__CUDACC_VER_BUILD__=0
+    CUDA_OPTIONS=-O3 --std=c++14 -D__NVCC__ -D__HIP__ -DCUDART_VERSION=11000 -D__CUDACC__ -D__CUDACC_VER_MAJOR__=11 -D__CUDACC_VER_MINOR__=0 -D__CUDACC_VER_BUILD__=0
     LIBS_SELECT=$(LIBS)
     CC=hipcc
     CUDA_CC_LINK=hipcc
 else
     CC=mpic++
     ifdef CUDA_ON_CPU
-        CUDA_CC=mpic++ -x c++ $(INCLUDE_PATH)
-        INCLUDE_PATH_NVCC=
+        CUDA_CC=mpic++ -x c++
+        INCLUDE_PATH_NVCC=$(INCLUDE_PATH)
         CUDA_CC_LINK=mpic++
-        CUDA_OPTIONS=-D__NVCC__ -DCUDART_VERSION=11000
-        LIBS_SELECT=$(LIBS)
+        CUDA_OPTIONS=-O3 --std=c++14 -D__NVCC__ -DCUDART_VERSION=11000
+        LIBS_SELECT=$(LIBS) -lboost_context
     else
         ifeq (, $(shell which nvcc))
-            CUDA_CC=mpic++ -x c++ $(INCLUDE_PATH)
+            CUDA_CC=tput setaf 1; echo "ERROR: NVCC not found; HIP and CUDA_ON_CPU disabled!"; tput sgr0 \#
             INCLUDE_PATH_NVCC=
-            CUDA_CC_LINK=mpic++
+            CUDA_CC_LINK=\#
             CUDA_OPTIONS=
-            LIBS_SELECT=$(LIBS)
+            LIBS_SELECT=
         else
+            INCLUDE_PATH_NVCC:=-Xcompiler=-Wno-deprecated-declarations $(INCLUDE_PATH_NVCC)
             CUDA_CC=nvcc -ccbin=mpic++
             CUDA_CC_LINK=nvcc -ccbin=mpic++
-            CUDA_OPTIONS=-use_fast_math  -arch=sm_61 -lineinfo --extended-lambda --expt-relaxed-constexpr 
+            CUDA_OPTIONS=-O3 --std=c++14 -use_fast_math  -arch=sm_61 -lineinfo --extended-lambda --expt-relaxed-constexpr
             LIBS_SELECT=$(LIBS_NVCC)
         endif
     endif
@@ -44,7 +46,7 @@ else
     CUDA_CC_LINK:=$(CUDA_CC_LINK)
 endif
 
-LDIR =
-OPT=
+OPT=-g -O3 --std=c++14
+INCLUDE_PATH:=-Wno-deprecated-declarations $(INCLUDE_PATH) 
 
 INCLUDE_PATH_NVCC:=$(INCLUDE_PATH_NVCC) -I./include 
